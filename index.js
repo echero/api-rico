a2ae89798d22b1c0bf1227aa3a7ab1682182e00a
Elyelin Carrasquero <elyelin15@gmail.com>
2022-05-30 23:17:08 -0300

    usando variables de entorno

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const express = require("express");
-const app = express();
-const port = 3000;
+const express = require("express"); //server-express
+const app = express();      //initialize app
+require('dotenv').config()
+const port = process.env.PORT || 8080   // establecemos nuestro puerto 
 
 const userRoute = require("./routes/User");
 const restaurantRoute = require("./routes/Restaurant");
@@ -12,6 +13,7 @@ app.use("/restaurant", restaurantRoute)
 // app.use("/restaurant/:id", restaurantRoute)
 
 app.use("/register", registerRouter)
+
 
 app.listen(port, () =>{
     console.log("Servidor corriendo en el puerto 3000");
