e4e5d65d1883ad12dde753b3efde068b4e361964
Elyelin Carrasquero <elyelin15@gmail.com>
2022-05-30 23:33:41 -0300

    .

--- index.js
+++ index.js
@@ -5,15 +5,11 @@ const port = process.env.PORT || 8080   // establecemos nuestro puerto
 
 const userRoute = require("./routes/User");
 const restaurantRoute = require("./routes/Restaurant");
-const registerRouter = require('./routes/Register')
 
 app.use("/user", userRoute);
 
 app.use("/restaurant", restaurantRoute)
 // app.use("/restaurant/:id", restaurantRoute)
-
-app.use("/register", registerRouter)
-
 
 app.listen(port, () =>{
     console.log("Servidor corriendo en el puerto 3000");
