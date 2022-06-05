548c92f2328ce674c8add4c18e7ca43eb413f4d9
Elyelin Carrasquero <elyelin15@gmail.com>
2022-06-05 16:34:25 -0300

    search controlador

--- controllers/SearchController.js
+++ controllers/SearchController.js
@@ -1,21 +1,32 @@
 let restaurants = [
-    {
-        "id": 1,
-        "conten": "Restaurant Rosa Negra",
-        "Direccion": "Dardor Rocha 1500",
-        "Estado" : true
-    },
-    {
-        "id": 2,
-        "conten": "Restaurant La Bisteca",
-        "Direccion": "Dardor Rocha 1000",
-        "Estado" : true 
-    }
-    
-]
+  {
+    id: 1,
+    conten: "Restaurant Rosa Negra",
+    direccion: "Dardor Rocha 1500",
+    estado: true,
+  },
+  {
+    id: 2,
+    conten: "Restaurant La Bisteca",
+    direccion: "Dardor Rocha 1000",
+    estado: true,
+  },
+];
 
 module.exports = {
-    get : (req, res) => {
-        res.json(restaurants)
+  get: async (req, res) => {
+      
+    const query = req.query.q.toLowerCase();
+    const filtered = restaurants.filter(r => { 
+      const name = r.conten.toLowerCase()
+      //retorno sí el nombre del restaurant convertido en minuscula  
+      //contiene la palbra que recibo por query string
+      return name.includes(query)
+    })
+
+    if(filtered.length === 0){
+      return res.status(404).json({message: "No hay resultados"})
     }
-}
+    return res.json(filtered);
+  },
+};
