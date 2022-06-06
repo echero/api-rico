d3661469eac1cd3b1c10ffa206cd02b52339cc1b
Elyelin Carrasquero <elyelin15@gmail.com>
2022-06-06 19:41:58 -0300

    controller search

--- controllers/SearchController.js
+++ controllers/SearchController.js
@@ -1,33 +1,3 @@
-// let restaurants = [
-//   {
-//     name: "Avenida Rivadavia 3439",
-//     direction: "El Pobre Luis",
-//     horario: "08am-22pm",
-//     tipoRestaurante: "Parrilla",
-//     telefono: 3232232,
-//   },
-//   {
-//     name: "Güerrín",
-//     direction: "Corrientes 1368",
-//     horario: "9am-23pm",
-//     tipoRestaurante: "Pizza",
-//     telefono: 8565446,
-//   },
-//   {
-//     name: "La Pescadorita",
-//     direction: "Humboldt 1905",
-//     horario: "9am-23pm",
-//     tipoRestaurante: "Mariscos",
-//     telefono: 798778,
-//   },
-//   {
-//     name: "La Parolaccia",
-//     direction: "Alicia Moureau de Justo 1052",
-//     horario: "10am-02am",
-//     tipoRestaurante: "Pastas",
-//     telefono: 356546,
-//   },
-// ];
 const restaurants = require('../Data/index')
 
 module.exports = {
