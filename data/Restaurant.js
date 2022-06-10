2eca482406b892037d87ec75efea2092e7949503
fernandounias <fernandouniascaminos@gmail.com>
2022-06-10 16:52:18 -0300

    Fav functionality and services layer added

--- data/Restaurant.js
+++ data/Restaurant.js
@@ -1,6 +1,5 @@
 const Restaurant = require('../models/restaurant')
 
-
 const Restaurantes = [
   new Restaurant (1, "Avenida Rivadavia 3439", "El Pobre Luis", "08am-22pm", "Parrilla", 3232232),
   new Restaurant (2, "Güerrín", "Corrientes 1368", "9am-23pm", "Pizza", 8565446),
