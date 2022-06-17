3e89bbb3f07addd020a165b2f57e3eabf0e08ef6
Elyelin Carrasquero <elyelin15@gmail.com>
2022-06-17 12:39:54 -0300

    data de reservation

---
+++ data/reservation.js
@@ -0,0 +1,9 @@
+const reservation = require('../models/reservation')
+
+const reservationes = [
+//   new reservation 
+//   new reservation
+//   new reservation 
+]
+
+module.exports = reservationes;
