1d5d541c5913f4a3e9d8f5558e95c1beeb677579
Elyelin Carrasquero <elyelin15@gmail.com>
2022-06-17 12:34:00 -0300

    busqueda por direccion

--- controllers/SearchController.js
+++ controllers/SearchController.js
@@ -1,18 +1,19 @@
-const restaurants = require('../data/Restaurant')
+const restaurants = require("../data/Restaurant");
 
 module.exports = {
+  //REGLA DE NEGOCIO <BUSQUEDA POR NOMBRE Y DIRECCION>
   get: async (req, res) => {
-      
     const query = req.query.q.toLowerCase();
-    const filtered = restaurants.filter(r => { 
-      const name = r.name.toLowerCase()
-      //retorno sí el nombre del restaurant convertido en minuscula  
+    const filtered = restaurants.filter((r) => {
+      const name = r.name.toLowerCase();
+      const direction = r.direction.toLowerCase();
+      //retorno sí el nombre del restaurant convertido en minuscula
       //contiene la palabra que recibo por query string
-      return name.includes(query)
-    })
+      return name.includes(query) || direction.includes(query);
+    });
 
-    if(filtered.length === 0){
-      return res.status(404).json({message: "No hay resultados"})
+    if (filtered.length === 0) {
+      return res.status(404).json({ message: "No hay resultados" });
     }
     return res.json(filtered);
   },
