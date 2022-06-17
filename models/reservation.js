2a218ce93077fba60fedf03f053951795a1612e3
Elyelin Carrasquero <elyelin15@gmail.com>
2022-06-17 12:34:28 -0300

    model de reservation

---
+++ models/reservation.js
@@ -0,0 +1,7 @@
+module.exports = function (id,idUser,idRestaurante,dia,hora) {
+    this.id = id
+    this.idUser = idUser
+    this.idRestaurante = idRestaurante
+    this.dia = dia
+    this.hora = hora
+}
